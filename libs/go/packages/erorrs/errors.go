30074a991a1848b4c3ac8470f51d6405593a2d75
Stepan Ananin <xrf848@gmail.com>
2025-11-22 23:53:48 +0300

    add Status error

---
+++ libs/go/packages/erorrs/errors.go
@@ -0,0 +1,29 @@
+package errs
+
+import "net/http"
+
+type Status struct {
+	status  int
+	message string
+}
+
+func (e *Status) Error() string {
+	return e.message
+}
+
+func (e *Status) Status() int {
+	return e.status
+}
+
+func NewStatusError(message string, status int) *Status {
+	return &Status{
+		message: message,
+		status: status,
+	}
+}
+
+var StatusInternalServerError = NewStatusError(
+	"Internal Server Error",
+	http.StatusInternalServerError,
+)
+
