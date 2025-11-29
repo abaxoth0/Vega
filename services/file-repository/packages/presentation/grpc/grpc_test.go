a5798c59b80f7c3af27ff1b162123d30102e3655
Stepan Ananin <xrf848@gmail.com>
2025-11-29 22:28:43 +0300

    add test suit for GetFileByPath RPC

--- services/file-repository/packages/presentation/grpc/grpc_test.go
+++ services/file-repository/packages/presentation/grpc/grpc_test.go
@@ -2,9 +2,12 @@ package grpc
 
 import (
 	"context"
+	"io"
+	"strconv"
 	"testing"
 	"time"
 	objectstorage "vega/packages/infrastructure/object-storage"
+	storageconnection "vega/packages/infrastructure/object-storage/connection"
 
 	file_repository "github.com/abaxoth0/Vega/common/protobuf/generated/go/services/file-repository"
 	"google.golang.org/grpc"
@@ -17,7 +20,7 @@ type closeFunc = func() error
 
 func new_grpc_clinet() (file_repository.FileRepositoryServiceClient, closeFunc) {
 	conn, err := grpc.NewClient(
-		"localhost:50001",
+		"localhost:"+strconv.Itoa(int(testPort)),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 	)
 	if err != nil {
@@ -28,20 +31,24 @@ func new_grpc_clinet() (file_repository.FileRepositoryServiceClient, closeFunc) 
 	return client, conn.Close
 }
 
-func newRequestContext() (context.Context, context.CancelFunc) {
+func newRPCContext() (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), time.Second*5)
 }
 
-func TestServer(t *testing.T) {
+// Creates gRPC server and clinet, after that pass this client into the handler function.
+// Handles connection/disconnection automatically.
+func withClient(t *testing.T, handler func(client file_repository.FileRepositoryServiceClient)) {
 	server, err := NewServer(objectstorage.Driver)
 	if err != nil {
 		t.Fatalf("Failed to create gRPC server: %v", err)
+		return
 	}
 
 	go func(){
 		t.Logf("gRPC server started")
 		if err := server.Start(testPort); err != nil {
 			t.Fatalf("Failed to start gRPC server: %v", err)
+			return
 		}
 		t.Logf("gRPC server stopped")
 	}()
@@ -51,6 +58,7 @@ func TestServer(t *testing.T) {
 		t.Logf("Stopping gRPC server...")
 		if err := server.Stop(); err != nil {
 			t.Fatalf("Failed to start gRPC server: %v", err)
+			return
 		}
 		t.Logf("Stopping gRPC server: DONE")
 	}()
@@ -58,16 +66,70 @@ func TestServer(t *testing.T) {
 	client, closeClient := new_grpc_clinet()
 	defer closeClient()
 
-	ctx, cancel := newRequestContext()
-	defer cancel()
+	handler(client)
+}
 
-	healthResp, err := client.HealthCheck(ctx, &file_repository.HealthCheckRequest{
-		Service: "file-repository",
+func TestServer(t *testing.T) {
+	withClient(t, func(client file_repository.FileRepositoryServiceClient) {
+		ctx, cancel := newRPCContext()
+		defer cancel()
+
+		healthResp, err := client.HealthCheck(ctx, &file_repository.HealthCheckRequest{
+			Service: "file-repository",
+		})
+		if err != nil {
+			t.Fatalf("HealthCheck RPC failed: %v", err)
+		}
+
+		t.Logf("Health check status: %s", healthResp.GetStatus())
+		t.Logf("Health check timestamp: %s", healthResp.GetTimestamp())
+	})
+}
+
+func TestRPC(t *testing.T) {
+	err := objectstorage.Driver.Connect(&storageconnection.Config{
+		URL:      "localhost:9000",
+		Login:    "minioadmin",
+		Password: "minioadmin",
+		Token:    "",
+		Secure:   false,
 	})
 	if err != nil {
-		t.Fatalf("Health check request failed: %v", err)
+		t.Fatalf("Failed to connect to object storage: %v", err)
 	}
+	defer func (){
+		if err := objectstorage.Driver.Disconnect(); err != nil {
+			t.Fatalf("Failed to disconnect from object storage")
+		}
+	}()
 
-	t.Logf("Health check status: %s", healthResp.GetStatus())
-	t.Logf("Health check timestamp: %s", healthResp.GetTimestamp())
+	t.Run("GetFileByPath()", func(t *testing.T) {
+		withClient(t, func(client file_repository.FileRepositoryServiceClient) {
+			ctx, cancel := newRPCContext()
+			defer cancel()
+
+			fileStream, err := client.GetFileByPath(ctx, &file_repository.GetFileByPathRequest{
+				Bucket: "test-bucket",
+				Path: "/file.txt",
+			})
+			if err != nil {
+				t.Fatalf("GetFileByPath() RPC failed: %v", err)
+			}
+			streaming := true
+			for streaming {
+				chunk, err := fileStream.Recv()
+				if err == io.EOF {
+					streaming = false
+				} else if err != nil {
+					t.Fatalf("File stream failed: %v", err)
+					break
+				}
+				if streaming && chunk == nil {
+					t.Fatalf("File stream failed: chunk is nil")
+					break
+				}
+				t.Logf("Chunk content: %v\n", string(chunk.GetContent()))
+			}
+		})
+	})
 }
